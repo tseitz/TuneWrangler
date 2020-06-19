e994578f4523ff94ad8799f04d57e3ce69eb331b
tseitz <tdseitz10@gmail.com>
2020-06-18 22:08:27 -0500

    Better prompts for corrupt playlist

--- FileAnalysis/src/checkCorruptedPlaylists.ts
+++ FileAnalysis/src/checkCorruptedPlaylists.ts
@@ -23,37 +23,40 @@ interface IPlaylistCount {
 
 const playlistDir = tw.checkOS('playlists', wsl)
 
-const exceptions = ['5 Stars', 'Archive', 'Bangers', 'Dont Sync']
+const exceptions = ['5 Stars', 'Archive', 'Bangers', 'Dont Sync', 'Unrated']
 
 async function run() {
-  // TODO: This sucks, figure out a way to await this properly
-  let oldPlaylistCount: IPlaylistCount[] = []
-  fs.readdir(playlistDir, async (e, files) => {
-    files.forEach(async (filename, index) => {
-      oldPlaylistCount.push(await checkFile(filename))
-    })
-  })
-
   prompt.start()
-  prompt.get(['hey'], () => {
-    let newPlaylistCount: IPlaylistCount[] = []
+  prompt.get(['Backup playlists and hit enter'], () => {
+    // TODO: This sucks, figure out a way to await this properly
+    let oldPlaylistCount: IPlaylistCount[] = []
     fs.readdir(playlistDir, async (e, files) => {
       files.forEach(async (filename, index) => {
-        newPlaylistCount.push(await checkFile(filename))
+        oldPlaylistCount.push(await checkFile(filename))
       })
     })
 
     prompt.start()
-    prompt.get(['hey'], (hey) => {
-      newPlaylistCount.forEach((newP) => {
-        oldPlaylistCount.forEach((oldP) => {
-          // arbitrary 2 difference to account for if I delete some from a playlist
-          if (
-            newP.playlistName === oldP.playlistName &&
-            newP.count < oldP.count
-          ) {
-            console.log(`Corrupted ${newP.playlistName}`)
-          }
+    prompt.get(['Sync phone, backup playlists again and hit enter'], (hey) => {
+      let newPlaylistCount: IPlaylistCount[] = []
+      fs.readdir(playlistDir, async (e, files) => {
+        files.forEach(async (filename, index) => {
+          newPlaylistCount.push(await checkFile(filename))
+        })
+      })
+
+      prompt.start()
+      prompt.get(['All done, you ready?'], (hey) => {
+        newPlaylistCount.forEach((newP) => {
+          oldPlaylistCount.forEach((oldP) => {
+            // arbitrary 2 difference to account for if I delete some from a playlist
+            if (
+              newP.playlistName === oldP.playlistName &&
+              newP.count < oldP.count
+            ) {
+              console.log(`Corrupted ${newP.playlistName}`)
+            }
+          })
         })
       })
     })
